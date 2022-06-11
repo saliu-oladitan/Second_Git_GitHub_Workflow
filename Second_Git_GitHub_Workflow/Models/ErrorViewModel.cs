03ae3e12ad7c894cf3a189508c53527ca0a11a2c
SALIU OLADITAN <sablik4all@gmail.com>
2022-06-11 17:08:42 +0100

    Add project files.

---
+++ Second_Git_GitHub_Workflow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Second_Git_GitHub_Workflow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
